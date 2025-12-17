22c79beb142a1d8ec5b9c098fc47634cfd5cdd18
Bit D13 <bit@d13.co>
2025-12-17 04:27:38 +0100

    skeleton; getValidatorStates

--- projects/reti-fast-sdk/smart_contracts/reti_reader/contract.algo.ts
+++ projects/reti-fast-sdk/smart_contracts/reti_reader/contract.algo.ts
@@ -1,7 +1,22 @@
-import { Contract } from '@algorandfoundation/algorand-typescript'
+import { Contract, log, uint64 } from "@algorandfoundation/algorand-typescript";
+import { Reti, ValidatorCurState } from "../reti/contract.algo";
+import { abimethod, compileArc4, encodeArc4, Uint16 } from "@algorandfoundation/algorand-typescript/arc4";
 
 export class RetiReader extends Contract {
-  hello(name: string): string {
-    return `Hello, ${name}`
+  @abimethod({ readonly: true, onCreate: 'require' })
+  getValidatorStates(registryAppId: uint64, validatorIds: uint64[]): ValidatorCurState {
+    for (const validatorId of validatorIds) {
+      const { returnValue } = compileArc4(Reti).call.getValidatorState({
+        appId: registryAppId,
+        args: [validatorId],
+      });
+      log(encodeArc4(returnValue));
+    }
+    return {
+      numPools: new Uint16(0),
+      totalStakers: 0,
+      totalAlgoStaked: 0,
+      rewardTokenHeldBack: 0,
+    };
   }
 }
